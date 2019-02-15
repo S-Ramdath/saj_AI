54f8521cb538178ab78d4ac30d2a1955f23eed22
S-Ramdath <43352059+S-Ramdath@users.noreply.github.com>
2019-02-14 21:49:43 -0400

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+std::string speech[];
+std::string input[];
+
+//simplifies responses.
+std::string talk(std::string a){
+std::cout<<a<<std::endl;
+}
+
+//simplifies recieving user input.
+std::string listen(std::string b){
+std::cin>>input>>std::endl;
+}
+
+
+int main(){
+talk(Hi. I am sajAI.);
+listen(input[0]);
+get.char();
+}
