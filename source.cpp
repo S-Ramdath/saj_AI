c7aeabb0af40fbd31b18b7d83dc827db039c49ba
S-Ramdath <43352059+S-Ramdath@users.noreply.github.com>
2019-02-14 22:09:20 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <string>
 
-std::string speech[];
-std::string input[];
+std::string speech;
+std::string input;
 
 //simplifies responses.
 std::string talk(std::string a){
-std::cout<<a<<std::endl;
+std::cout<<a;
 }
 
 //simplifies recieving user input.
 std::string listen(std::string b){
-std::cin>>input>>std::endl;
+getline(std::cin,input);
+}
+
+//easy way to make a new line.
+void newLine(){
+std::cout<<"\n";
 }
 
 //char main just because i can lol.
-char main(){
-talk(Hi. I am sajAI. You may respond now.);
-listen(input[0]);
-get.char();
-return 'a';
+int main(){
+talk("Hi. I am sajAI. You may respond now.");
+newLine();
+listen(input);
+talk("You said: ");
+talk(input);
+return 0;
 }
